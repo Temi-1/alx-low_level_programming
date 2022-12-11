6793bc1a8c91f955677ca1e14ba732add34da213
Temi-1 <temiapantaku@gmail.com>
2022-12-11 07:26:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/** 
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+/**
+ * main - prints desired sentence
+ * Return: 0 if successful
  */
+
 int main(void)
 {
-	puts("\"Programming is like building ba multilingual puzzle");
-	return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
