d252743b2cdc3feaeafa2648c557035c5e70f36e
Temi-1 <temiapantaku@gmail.com>
2022-12-12 15:59:53 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c!= 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return(0)
+}
