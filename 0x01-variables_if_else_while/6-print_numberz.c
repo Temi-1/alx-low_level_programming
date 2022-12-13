846718df37c51113a32c8a827842628d29609a5f
Temi-1 <temiapantaku@gmail.com>
2022-12-12 16:40:13 -0800

    tyask 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 
 /**
  * main - entry point
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = '0'; num < '10'; num++)
+	for (num = 0; num < 10; num++)
 		putchar(num + '0');
 	putchar('\n');
 	return (0);
