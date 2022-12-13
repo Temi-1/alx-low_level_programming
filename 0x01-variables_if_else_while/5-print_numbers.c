1566113cd02bb6ba22206ac9831a09938f8d59a6
Temi-1 <temiapantaku@gmail.com>
2022-12-12 16:16:42 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+ * main - entry point
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	purchar('\n');
+	return (0)
+}
