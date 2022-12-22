cdf27745262dc903c24737066e437d6ba58e698a
Temi-1 <temiapantaku@gmail.com>
2022-12-22 08:37:47 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,26 +2,22 @@
 /**
  * _strncpy - copy a string
  * @dest: input value
- * @src: input value
- * @n: input value
+ * @src: source string
+ * @n: maximum number of bytes
  *
- * Return: dest
+ * Return: pointer to resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int index = 0, src_len = 0;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++
-	}
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
